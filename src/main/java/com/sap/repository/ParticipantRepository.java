7fed7df318316805230c4f1756e982e89fb2f2a3
Michał Drzewiecki <michal.drzewiecki@sap.com>
2017-12-15 15:23:34 +0100

    removes previous version of the source code

--- src/main/java/com/sap/repository/ParticipantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sap.repository;
-
-import com.sap.model.Participant;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ParticipantRepository extends CrudRepository<Participant, Long>
-{
-	Participant findFirstByNameAndSurname(final String name, final String surname);
-}
